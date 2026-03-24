657ec20ccefcf7386bd91ce6425d1c34e9e3b7e4
Romontt <rmontiel1994@gmail.com>
2026-03-24 11:38:27 -0600

    Update lux_injector.js

--- patrocinios/Lux/lux_injector.js
+++ patrocinios/Lux/lux_injector.js
@@ -28,7 +28,7 @@ const LuxPatrocinio = {
             .banner-lux-pc {
                 display: none;
                 position: fixed !important;
-                left: 15px !important;
+                left: 05px !important;
                 top: 50% !important;
                 transform: translateY(-50%);
                 width: 130px;
